72d99c614726b7e9096ec849512d4cfe4a02210c
LouisManchon <10578@holbertonstudents.com>
2025-12-04 21:22:15 +0100

    Réorganisation structure Django

---
+++ backend/animaux/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+app_name = 'animaux'
+
+urlpatterns = [
+    # Page d'accueil
+    path('', views.accueil, name='accueil'),
+
+    # Liste des animaux
+    path('nos-animaux/', views.nos_animaux, name='nos_animaux'),
+
+    # Détails d'un animal
+    path('animal/<int:id>/', views.detail_animal, name='detail_animal'),
+
+    # Formulaire d'adoption
+    path('formulaire-adoption/<int:id>/', views.formulaire_adoption, name='formulaire_adoption')
+]
