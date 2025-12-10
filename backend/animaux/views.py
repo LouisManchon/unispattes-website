c9383e4596b0595981395b0b0b23bd488305e36b
LouisManchon <10578@holbertonstudents.com>
2025-12-10 13:27:57 +0100

    add doctring for animaux/views.py

--- backend/animaux/views.py
+++ backend/animaux/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 
 def index(request):
+    """Page d'accueil"""
     return render(request, 'animaux/index.html')
 
 def nos_animaux(request):
+    """Page listant tous les animaux"""
     return render(request, 'animaux/nos_animaux.html')
