f3767501bcf20a62569555c43dba2a3654e843b2
LouisManchon <10578@holbertonstudents.com>
2025-12-10 13:26:04 +0100

    create views for index and nos_animaux html

--- backend/animaux/views.py
+++ backend/animaux/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'animaux/index.html')
+
+def nos_animaux(request):
+    return render(request, 'animaux/nos_animaux.html')
