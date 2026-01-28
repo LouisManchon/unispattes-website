fc1ba29dc741b142e142d97798626460c984d661
LouisManchon <10578@holbertonstudents.com>
2026-01-28 20:03:26 +0100

    fix commentary

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings  # ← ✅ AJOUTE CETTE LIGNE
-from django.conf.urls.static import static  # ← ✅ ET CELLE-CI
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,6 +25,6 @@ urlpatterns = [
     path('users/', include('users.urls')),
 ]
 
-# ✅ Servir les fichiers média en mode développement
+# Servir les fichiers média en mode développement
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
