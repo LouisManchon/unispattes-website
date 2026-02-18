967900b18a298ba74d7cb5e84c5b043dc03a462c
LouisManchon <10578@holbertonstudents.com>
2026-02-18 16:49:13 +0100

    add commentary in animaux/urls.py

--- backend/animaux/urls.py
+++ backend/animaux/urls.py
@@ -7,11 +7,12 @@ urlpatterns = [
     # Page d'accueil
     path('', views.index, name='accueil'),
 
-    # Liste des animaux
+    # Page affichant tous les animaux disponibles
     path('nos-animaux/', views.nos_animaux, name='nos_animaux'),
 
-    # Détails d'un animal
+    # Fiche détaillée d’un animal
     path('animal/<int:id>/', views.detail_animal, name='detail_animal'),
 
+    # Page de présentation du refuge
     path('a-propos/', views.a_propos, name='a_propos'),
 ]
